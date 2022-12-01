a19b8b1e10e655ef693c9fa42283c54c2abe9d86
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2022-12-01 14:24:11 +0100

    first commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Sergey on 1.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
